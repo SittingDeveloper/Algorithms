1608d7c56a1979af2a23a867813b795ef5bce307
SittingDeveloper <rlaaudtlr145@gmail.com>
2022-12-15 16:59:57 +0900

    백준 11659 (Silver4) 구간 합 구하기 4

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -1,73 +1,13 @@
 package Practice;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-
-    static int N, E, V;
-    static boolean dfs_visited[] = new boolean[1001];
-    static int Graph[][] = new int[1001][1001];
-
-    static void dfs(int node) {
-
-        dfs_visited[node] = true;
-
-        System.out.print(node + " ");
-
-        for (int next = 0; next <= N; ++next) {
-            if (dfs_visited[next] == false && Graph[next][node] == 1) {
-                dfs(next);
-            }
-        }
-
-    }
-
-    static void bfs(int node) {
-
-        boolean[] bfs_visited = new boolean[1001];
-        bfs_visited[node] = true;
-
-        Queue<Integer> myQueue = new LinkedList<>();
-        myQueue.add(node);
-
-        while (!myQueue.isEmpty()) {
-            int curr = myQueue.remove();
-            System.out.print(curr + " ");
-
-            for (int next = 0; next <= N; ++next) {
-                if (bfs_visited[next] == false && Graph[curr][next] == 1) {
-                    bfs_visited[next] = true;
-                    myQueue.add(next);
-                }
-            }
-
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        N = scan.nextInt();
-        E = scan.nextInt();
-        V = scan.nextInt();
-
-        for (int i = 0; i < E; i++) {
-            int u = scan.nextInt();
-            int a = scan.nextInt();
-
-            Graph[u][a] = Graph[a][u] = 1;
-        }
-
-        dfs(V);
-        System.out.println("");
-        bfs(V);
-
-    }
 
 
 }
