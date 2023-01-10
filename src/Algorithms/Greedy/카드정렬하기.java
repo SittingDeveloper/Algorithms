d0ff5456d5ca84507cab29efd2b5bb0f8a2d8b51
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-01-10 16:50:26 +0900

    백준 1715 (골드5) 카드정렬하기 (그리디)

--- src/Algorithms/Greedy/카드정렬하기.java
+++ src/Algorithms/Greedy/카드정렬하기.java
@@ -1,12 +1,12 @@
-package Practice;
+package Algorithms.Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-public class Main {
+// 1715
+public class 카드정렬하기 {
 
     public static void main(String[] args) throws IOException {
 
--- src/BaekJoon/Code_1715.java
+++ src/BaekJoon/Code_1715.java
@@ -1,12 +1,11 @@
-package Practice;
+package BaekJoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Code_1715 {
 
     public static void main(String[] args) throws IOException {
 
@@ -14,7 +13,6 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        // PriorityQueue 는 add 순서 상관없이 (작은 수) 부터 뺌
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
@@ -25,15 +23,12 @@ public class Main {
 
         while (pq.size() != 1) {
 
-            // 현재 카드의 개수가 가장 작은 묶음 2개를 선택해 합침
             int data1 = pq.remove();
             int data2 = pq.remove();
             sum += data1 + data2;
 
-            // 합친 카드 묶음을 다시 전체 카드 묶음 속에 넣음
             pq.add(data1 + data2);
 
-            // 위 과정을 카드 묶음이 1개만 남을 때 까지 반복함
         }
 
         System.out.println(sum);
