ae654428bec28de99907c07a082f211ce78e98a5
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-03-01 14:55:25 +0900

    백준 1120 (Silver4) 문자열 (문자열)

--- src/Algorithms/String/문자열.java
+++ src/Algorithms/String/문자열.java
@@ -1,14 +1,12 @@
-package Practice;
+package Algorithms.String;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
+// 1120
+public class 문자열 {
 
     public static void main(String[] args) throws IOException {
 
@@ -33,9 +31,11 @@ public class Main {
 
         int min = Integer.MAX_VALUE;
 
+        // A가 B보다 작은 값 만큼 반복, B : 6, A : 4 ? 2회 반복
         for (int i = 0; i <= diff; i++) {
             int count = 0;
             for (int j = 0; j < A_arr.length; j++) {
+                // 각 자리 비교
                 if (A_arr[j] != B_arr[j + i]) {
                     count++;
                 }
