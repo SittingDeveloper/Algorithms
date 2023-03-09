fd50e13dcd9a5701e584ebac42d1b2fc41f0ff74
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-03-09 18:35:32 +0900

    프로그래머스 (Level2)  (DFS) 게임 맵 최단거리

--- src/Algorithms/Search/BFS/Programmers_게임_맵_최단거리.java
+++ src/Algorithms/Search/BFS/Programmers_게임_맵_최단거리.java
@@ -1,44 +1,54 @@
-package Practice;
+package Algorithms.Search.BFS;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+// BFS 탐색
+// https://school.programmers.co.kr/learn/courses/30/lessons/1844
+public class Programmers_게임_맵_최단거리 {
 
-    static int maps[][] = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
     static boolean visited[][];
+    static int maps[][] = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
+
     static int dx[] = {0, 0, 1, -1};
-    static int dy[] = {-1, 1, 0, 0};
+    static int dy[] = {1, -1, 0, 0};
 
     public static void main(String[] args) {
 
-        visited = new boolean[maps.length][maps.length];
+        int answer = 0;
+        visited = new boolean[maps.length][maps[0].length];
 
-        visited[0][0] = true;
         bfs(0, 0);
 
-        System.out.println(maps[maps.length - 1][maps.length - 1]);
+        if (maps[maps.length - 1][maps[0].length - 1] == 1) {
+            answer = -1;
+        } else {
+            answer = maps[maps.length - 1][maps[0].length - 1];
+        }
+
+        System.out.println(answer);
 
     }
 
     static void bfs(int startX, int startY) {
 
+        visited[startX][startY] = true;
+
         Queue<Move> q = new LinkedList<>();
         q.add(new Move(startX, startY));
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
 
             Move now = q.poll();
             int nowX = now.x;
             int nowY = now.y;
 
             for (int i = 0 ; i < 4 ; i++) {
+
                 int nextX = nowX + dx[i];
                 int nextY = nowY + dy[i];
 
-                if (nextX < 0 || nextX >= maps.length || nextY < 0 || nextY >= maps.length) {
+                if (nextX < 0 || nextY < 0 || nextX >= maps.length || nextY >= maps[0].length) {
                     continue;
                 }
 
@@ -55,11 +65,10 @@ public class Main {
     }
 
     static class Move {
-        int x = 0;
-        int y = 0;
-
-        Move(int x, int y) {
-            this.x =x;
+        int x;
+        int y;
+        Move (int x, int y) {
+            this.x = x;
             this.y = y;
         }
     }
