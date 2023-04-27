8bbd6e6c98b5351ff27178a2a1fad24a36c72953
SittingDeveloper <rlaaudtlr145@gmail.com>
2023-04-27 17:50:07 +0900

    SW Expert (Level2) (Greedy) 백만장자 프로젝트

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -7,54 +7,39 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int stones[] = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
-        int k = 3;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int testCase = Integer.parseInt(br.readLine());
 
-        int left = 1;
-        int right = 200000000;
+        for (int t = 0; t < testCase; t++) {
 
-        int max = 0;
+            int N = Integer.parseInt(br.readLine());
+            int cost[] = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        while(left <= right){
-            int mid = (left + right) / 2;
-
-            if(!cross(stones, k, mid)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-                max = Math.max(max, mid);
-            }
-        }
-
-        System.out.println(left);
-
-    }
-
-    public static boolean cross(int[] stones, int k, int mid) {
-        int cnt = 0;
-
-        for (int stone : stones) {
-
-            // 0보다 작아서 못 건너가면 cnt++
-            if (stone - mid < 0) {
-                cnt++;
+            for (int i = 0; i < N; i++) {
+                cost[i] = Integer.parseInt(st.nextToken());
             }
 
-            // 0이상이면 건널 수 있음 -> cnt 0으로 다시 갱신
-            else {
-                cnt = 0;
+            int ans = 0;
+            int max = cost[N - 1];
+
+            for (int i = N - 1; i >= 0; i--) {
+                if (cost[i] < max) {
+                    ans += max - cost[i];
+                } else {
+                    max = Math.max(cost[i], max);
+                }
             }
 
-            // 못 건너서 쌓인 cnt 의 값이 건널 수 있는 최대 칸 수 k를 넘으면 현재 mid 값으로는 건널 수 없음
-            if (cnt == k) {
-                return false;
-            }
+            sb.append(ans + "\n");
 
         }
 
-        return true;
+        System.out.println(sb);
+
     }
 
 
